d7079b30d80fc295b27a20a4dae8613c62543960
mafora12 <123818265+mafora12@users.noreply.github.com>
2024-09-23 10:32:59 -0500

    Update Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Facturacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crea una nueva instancia de la clase Restaurante
+            Restaurante restaurante = new Restaurante();
+            bool continuar = true;
